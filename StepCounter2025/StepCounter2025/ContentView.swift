0bb868724d0f0a6189a281d99ed5fe05506bdc9e
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-08-13 08:36:08 -0400

    StepCounter2025 initial commit

---
+++ StepCounter2025/StepCounter2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepCounter2025
+//
+//  Created by Thomas Cowern on 8/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepCounter2025/StepCounter2025/StepCounter2025App.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounter2025App.swift
+//  StepCounter2025
+//
+//  Created by Thomas Cowern on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StepCounter2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
